b73d4ca3ae441f98cc74ca3fce903b31af1fa094
briangay <gay.b@northeastern.edu>
2023-12-12 12:27:55 -0500

    client change

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 const COURSES_URL = "http://localhost:4000/api/Courses";
 
-const API_BASE = process.env.REACT_APP_API_BASE;
-const MODULES_URL = `${API_BASE}/modules`;
+//const API_BASE = process.env.REACT_APP_API_BASE;
+//const MODULES_URL = `${API_BASE}/modules`;
 
+const MODULES_URL = "http://localhost:4000/api/modules";
 export const updateModule = async (module) => {
     const response = await axios.
     put(`${MODULES_URL}/${module._id}`, module);
